fe8198335fa28f2270c0add3dbcbf8a96057ac68
OutPizzaTheHutt <143542173+OutPizzaTheHutt@users.noreply.github.com>
2023-10-05 14:15:43 -0500

    Add files via upload

--- K_Y_stats_tool.py
+++ K_Y_stats_tool.py
@@ -1,8 +1,7 @@
 import K_Y_stats_module
 numbers = []
-
 while True:
-    print("\nOptions:")
+    print("Options:")
     print("1. Input a number")
     print("2. Find the Mean")
     print("3. Find the Variance")
@@ -12,32 +11,32 @@ while True:
     print("7. Summary of Statistics")
     print("When finished, enter (done)")
     
-    choice = input("Enter your choice: ")
-    if choice == '1':
+    option = input("Enter your option: ")
+    if option == '1':
         try:
             num = float(input("Enter a number: "))
             numbers.append(num)
         except ValueError:
             print("Invalid input. Please enter a valid number." )
-    elif choice == '2':
+    elif option == '2':
         mean = K_Y_stats_module.calculate_mean(numbers)
         print(f"Mean: {mean}")
-    elif choice == '3':
+    elif option == '3':
         variance = K_Y_stats_module.calculate_variance(numbers)
-    elif choice == '4':
+    elif option == '4':
         std_deviation = K_Y_stats_module.calculate_std_deviation(numbers)
         print(f"Standard Deviation: {std_deviation}")
-    elif choice == '5':
+    elif option == '5':
         std_error = K_Y_stats_module.calculate_std_error(numbers)
         print(f"Standard Error: {std_error}")
-    elif choice == '6':
+    elif option == '6':
         try:
             new_value = float(input("Enter a new value: "))
             z_score = K_Y_stats_module.calculate_z_score(numbers, new_value)
             print(f"Z-score: {z_score}")
         except ValueError:
             print("Invalid input. Please enter a valid number.")
-    elif choice == '7':
+    elif option == '7':
         mean = K_Y_stats_module.calculate_mean(numbers)
         variance = K_Y_stats_module.calculate_variance(numbers)
         std_deviation = K_Y_stats_module.calculate_std_deviation(numbers)
@@ -47,7 +46,7 @@ while True:
         print(f"Standard Deviation: {std_deviation}")
         print(f"Standard Error: {std_error}")
         
-    elif choice == 'done':
+    elif option == 'done':
         break
     else:
         print("Wrong choice, select a valid option")
